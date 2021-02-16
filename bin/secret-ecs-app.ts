68c04d556b98878e3866373fddd92ff996388242
Michael Tuszynski <tuszym@amazon.com>
2021-02-15 16:11:31 -0800

    cleanup

--- bin/secret-ecs-app.ts
+++ bin/secret-ecs-app.ts
@@ -2,26 +2,27 @@ import { App } from '@aws-cdk/core';
 import { VPCStack } from '../lib/vpc-stack';
 import { RDSStack } from '../lib/rds-stack';
 import { ECSStack } from '../lib/ecs-stack';
-import { config } from "dotenv";
-config();
+
+const cdkEnv = { 
+    account: process.env.CDK_DEPLOY_ACCOUNT || process.env.AWS_ACCOUNT_ID, 
+    region: process.env.CDK_DEPLOY_REGION || process.env.AWS_REGION 
+}
 
 const app = new App();
 
-const VPC_NAME = "DemoVPCSMFargate";
-const DB_NAME = "tododb"
-
-const vpcStack = new VPCStack(app, 'VPCStack', {
-    vpcName: VPC_NAME
+const vpcStack = new VPCStack(app, 'VPCStack',{
+    env: cdkEnv
 });
 
 const rdsStack = new RDSStack(app, 'RDSStack', {
     vpc: vpcStack.vpc,
-    dbName: DB_NAME
+    env: cdkEnv
 });
 
 const ecsStack = new ECSStack(app, "ECSStack", {
     vpc: vpcStack.vpc,
-    dbSecretArn: rdsStack.dbSecret.secretArn
+    dbSecretArn: rdsStack.dbSecret.secretArn,
+    env: cdkEnv
 });
 
 rdsStack.addDependency(vpcStack);
--- lib/rds-stack.ts
+++ lib/rds-stack.ts
@@ -1,12 +1,12 @@
 import { App, StackProps, Stack, CfnOutput } from "@aws-cdk/core";
-import { DatabaseSecret, DatabaseInstance, DatabaseInstanceEngine, 
-         PostgresEngineVersion, Credentials, StorageType
-       } from '@aws-cdk/aws-rds';
-import { Vpc, Port, SubnetType, InstanceType, InstanceClass, InstanceSize } from '@aws-cdk/aws-ec2';
+import {
+    DatabaseSecret, DatabaseInstance, DatabaseInstanceEngine,
+    PostgresEngineVersion, Credentials, StorageType
+} from '@aws-cdk/aws-rds';
+import { Vpc, Port, SubnetType, InstanceType } from '@aws-cdk/aws-ec2';
 
 export interface RDSStackProps extends StackProps {
-    vpc: Vpc,
-    dbName: string,
+    vpc: Vpc
 }
 
 export class RDSStack extends Stack {
@@ -17,35 +17,34 @@ export class RDSStack extends Stack {
     constructor(scope: App, id: string, props: RDSStackProps) {
         super(scope, id, props);
 
-        const databaseUsername = 'postgres';
-        const port = 5432;
+        const dbUser = this.node.tryGetContext("dbUser");
+        const dbName = this.node.tryGetContext("dbName");
+        const dbPort = this.node.tryGetContext("dbPort");
+        const dbInstanceType = this.node.tryGetContext("instanceType");
 
         this.dbSecret = new DatabaseSecret(this, 'DbSecret', {
-            username: databaseUsername
+            username: dbUser
         });
-       
+
         this.postgresRDSInstance = new DatabaseInstance(this, 'Postgres-rds-instance', {
             engine: DatabaseInstanceEngine.postgres({
                 version: PostgresEngineVersion.VER_12_4
             }),
-            instanceType: InstanceType.of(
-                InstanceClass.T2,
-                InstanceSize.MICRO
-            ),
+            instanceType: new InstanceType(dbInstanceType),
             vpc: props.vpc,
             vpcSubnets: { subnetType: SubnetType.PUBLIC },
-            storageEncrypted: false, 
+            storageEncrypted: false,
             multiAz: false,
             autoMinorVersionUpgrade: false,
             allocatedStorage: 25,
             storageType: StorageType.GP2,
             deletionProtection: false,
-            credentials: Credentials.fromSecret(this.dbSecret,databaseUsername),
-            databaseName: props.dbName,
-            port: port,
+            credentials: Credentials.fromSecret(this.dbSecret, dbUser),
+            databaseName: dbName,
+            port: dbPort,
         });
-        
-        this.postgresRDSInstance.connections.allowFromAnyIpv4(Port.tcp(port));
+
+        this.postgresRDSInstance.connections.allowFromAnyIpv4(Port.tcp(dbPort));
 
         new CfnOutput(this, 'POSTGRES_URL', { value: this.postgresRDSInstance.dbInstanceEndpointAddress });
 
