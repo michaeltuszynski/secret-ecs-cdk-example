f0fbdafb783a768f8a8eb2b1c9a34cd3c0ffb446
Michael Tuszynski <tuszym@amazon.com>
2021-03-01 14:25:46 -0800

    added tests and refactor cleaner code

--- lib/vpc-stack.ts
+++ lib/vpc-stack.ts
@@ -1,17 +1,34 @@
-import { App, Stack, StackProps } from '@aws-cdk/core';
-import { Vpc } from '@aws-cdk/aws-ec2'
+import { App, Stack, StackProps, Construct } from '@aws-cdk/core';
+import { Vpc, SubnetType } from '@aws-cdk/aws-ec2'
+
+export interface VpcProps extends StackProps {
+    maxAzs: number;
+}
 
 export class VPCStack extends Stack {
     readonly vpc: Vpc;
 
-    constructor(scope: App, id: string, props: StackProps) {
+    constructor(scope: Construct, id: string, props: VpcProps) {
         super(scope, id, props);
 
-        const vpcName = scope.node.tryGetContext("vpcName");
+        if (props.maxAzs !== undefined && props.maxAzs <= 1) {
+            throw new Error('maxAzs must be at least 2.');
+        }
 
-        this.vpc = new Vpc(this, `${vpcName}`, {
-            cidr: '10.0.0.0/16',
-            maxAzs: 2
-        })
+        this.vpc = new Vpc(this, 'ecsWorkshopVPC', {
+            cidr: "10.0.0.0/16",
+            subnetConfiguration: [
+                {
+                    cidrMask: 24,
+                    name: 'public',
+                    subnetType: SubnetType.PUBLIC,
+                },
+                {
+                    cidrMask: 24,
+                    name: 'private',
+                    subnetType: SubnetType.PRIVATE,
+                },
+            ],
+        });
     }
 }
