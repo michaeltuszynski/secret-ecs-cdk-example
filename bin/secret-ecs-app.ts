9ac7098c14ebd8f59454ba7de306aa957f6f4f9c
Michael Tuszynski <tuszym@amazon.com>
2021-02-28 17:11:16 -0800

    removed env var

--- bin/secret-ecs-app.ts
+++ bin/secret-ecs-app.ts
@@ -3,20 +3,12 @@ import { VPCStack } from '../lib/vpc-stack';
 import { RDSStack } from '../lib/rds-stack';
 import { ECSStack } from '../lib/ecs-fargate-stack';
 
-const cdkEnv = {
-    account: process.env.CDK_DEPLOY_ACCOUNT || process.env.AWS_ACCOUNT_ID,
-    region: process.env.CDK_DEPLOY_REGION || process.env.AWS_REGION
-}
-
 const app = new App();
 
-const vpcStack = new VPCStack(app, 'VPCStack', {
-    env: cdkEnv
-});
+const vpcStack = new VPCStack(app, 'VPCStack', {});
 
 const rdsStack = new RDSStack(app, 'RDSStack', {
     vpc: vpcStack.vpc,
-    env: cdkEnv
 });
 
 rdsStack.addDependency(vpcStack);
@@ -24,7 +16,6 @@ rdsStack.addDependency(vpcStack);
 const ecsStack = new ECSStack(app, "ECSStack", {
     vpc: vpcStack.vpc,
     dbSecretArn: rdsStack.dbSecret.secretArn,
-    env: cdkEnv
 });
 
 ecsStack.addDependency(rdsStack);
